ba9afec63d40678f8d1d1090a2f68d899e1c75a7
Kevin <kevinjunchan@gmail.com>
2023-10-25 17:00:15 +0100

    added settings screen

--- App.js
+++ App.js
@@ -4,18 +4,21 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CalendarContainer from './screens/CalendarContainer';
 import HomepageContainer from './screens/HomepageContainer';
+import SettingsContainer from './screens/SettingsContrainer';
 
 const Tab = createBottomTabNavigator();
-const homeName = "Today's Tasks"
+const homeName = "Tasks"
 const calendarName= "Calendar"
+const settingsName= "Settings"
 
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator initialRouteName = {homeName}>
-        <Tab.Screen name = {homeName} component={HomepageContainer} />
         <Tab.Screen name = {calendarName} component={CalendarContainer} />
+        <Tab.Screen name = {homeName} component={HomepageContainer} />
+        <Tab.Screen name = {settingsName} component={SettingsContainer} />
       </Tab.Navigator>
     </NavigationContainer>
   );
---
+++ screens/SettingsContrainer.js
@@ -0,0 +1,11 @@
+import { View } from "react-native"
+
+const SettingsContainer = () => {
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+export default SettingsContainer
